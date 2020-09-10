88e36a0d09c5143761bcb93e275468290bca4ab0
like <like@ihomefnt.com>
2020-09-10 16:14:37 +0800

    fix

--- devtool-example/src/test/java/com/fuyo/cloud/db/MapperTest.java
+++ devtool-example/src/test/java/com/fuyo/cloud/db/MapperTest.java
@@ -1,16 +1,9 @@
 package com.fuyo.cloud.db;
 
-import com.fuyo.cloud.common.utils.Mysql;
-import com.fuyo.cloud.db.biz.exam.g.dao.GExamOrderDtoMapper;
-import com.fuyo.cloud.db.biz.exam.g.dao.GExamOrderProductsDtoMapper;
-import com.fuyo.cloud.db.biz.exam.g.domain.GExamOrderDto;
-import com.fuyo.cloud.db.biz.exam.g.domain.GExamOrderDtoExample;
-import com.fuyo.cloud.db.biz.exam.g.domain.GExamOrderProductsDto;
-import com.fuyo.cloud.db.biz.exam.g.domain.GExamOrderProductsDtoExample;
-import com.fuyo.cloud.db.biz.mall.g.dao.GGoodsDtoMapper;
-import com.fuyo.cloud.db.biz.mall.g.dao.GGoodsProductDtoMapper;
-import com.fuyo.cloud.db.biz.mall.g.domain.GGoodsProductDto;
-import com.fuyo.cloud.db.biz.mall.g.domain.GGoodsProductDtoExample;
+
+import com.fuyo.cloud.db.biz.test.g.dao.GTestDtoMapper;
+import com.fuyo.cloud.db.biz.test.g.domain.GTestDto;
+import com.fuyo.cloud.db.biz.test.g.domain.GTestDtoExample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,121 +21,58 @@ import java.util.Map;
 public class MapperTest {
 
     @Resource
-    private GExamOrderDtoMapper gExamOrderDtoMapper;
-
-    @Resource
-    private GExamOrderProductsDtoMapper gExamOrderProductsDtoMapper;
-
-    @Test
-    public void test1() {
-
-        List<Map> maps = gExamOrderDtoMapper.selectToMap(
-                GExamOrderDtoExample.build()
-                        .andCreatedTimeBetween(
-                                LocalDateTime.now().plusMonths(-12L),
-                                LocalDateTime.now()
-                        )
-                        .example()
-                        .groupBy(GExamOrderDto.Column.createdTime.yearWeekMondayFirst())
-                ,
-                GExamOrderDto.Column.createdTime.firstDayOfWeekAs(),
-                "COUNT(1) as count"
-        );
+    private GTestDtoMapper gTestDtoMapper;
 
 
-    }
-
-
-    @Test
-    public void test2() {
-
-
+//    @Test
+//    public void test3() {
+//        GTestDtoExample.Criteria joinCriteria = GTestDtoExample.build()
+////                .andUserIdEqualTo(1)
+//                ;
 //
-//        criteria.createJoinCriteria()
-//                .innerJoinTable(urExample)
-//                //使用User表的user_id关联user_role表的user_id
-//                .on(a->a.getUserId(), a->a.equalTo(b->b.tableInfo.getUserId()));
+//
+//        List<Map> maps = gTestDtoMapper.selectToMap(
+//                GTestDtoExample.build()
+//                        .andBetween(
+//                                GTestDto.Column.createdTime.withAlias(),
+//                                LocalDateTime.now().plusMonths(-1L),
+//                                LocalDateTime.now()
+//                        )
+//                        .example()
+//                        .leftJoin(GTestDto.TABLE,
+//                                Mysql.equal(GTestDto.Column.orderId.withAlias(), GTestDto.Column.id.withAlias()))
+//                        .andCriteria(joinCriteria)
+//                        .groupBy(GTestDto.Column.name.withAlias())
+//                        .orderBy("total desc")
+//                ,
+//                GTestDto.Column.name.withAlias("name"),
+//                Mysql.sum(GTestDto.Column.quantity.withAlias(), "total")
+//        );
+//
+//        System.out.println(maps);
+//
+//    }
 
-
-        List<Map> maps = gExamOrderProductsDtoMapper.selectToMap(
-                GExamOrderProductsDtoExample.build()
-                        .andBetween(
-                                GExamOrderDto.Column.createdTime.withAlias(),
-                                LocalDateTime.now().plusMonths(-12L),
-                                LocalDateTime.now()
-                        )
-                        .example()
-                        .leftJoin(GExamOrderDto.TABLE,
-                                Mysql.equal(GExamOrderProductsDto.Column.orderId.withAlias(), GExamOrderDto.Column.id.withAlias()))
-                        .groupBy(GExamOrderProductsDto.Column.name.withAlias())
-                        .orderBy("total desc")
-                ,
-                GExamOrderProductsDto.Column.name.withAlias("name"),
-                Mysql.sum(GExamOrderProductsDto.Column.quantity.withAlias(), "total")
-        );
-
-
-        System.out.println(maps);
-
-    }
-
-
-    @Test
-    public void test3() {
-        GExamOrderDtoExample.Criteria joinCriteria = GExamOrderDtoExample.build()
-//                .andUserIdEqualTo(1)
-                ;
-
-
-        List<Map> maps = gExamOrderProductsDtoMapper.selectToMap(
-                GExamOrderProductsDtoExample.build()
-                        .andBetween(
-                                GExamOrderDto.Column.createdTime.withAlias(),
-                                LocalDateTime.now().plusMonths(-1L),
-                                LocalDateTime.now()
-                        )
-                        .example()
-                        .leftJoin(GExamOrderDto.TABLE,
-                                Mysql.equal(GExamOrderProductsDto.Column.orderId.withAlias(), GExamOrderDto.Column.id.withAlias()))
-                        .andCriteria(joinCriteria)
-                        .groupBy(GExamOrderProductsDto.Column.name.withAlias())
-                        .orderBy("total desc")
-                ,
-                GExamOrderProductsDto.Column.name.withAlias("name"),
-                Mysql.sum(GExamOrderProductsDto.Column.quantity.withAlias(), "total")
-        );
-
-        System.out.println(maps);
-
-    }
-
-
-    @Resource
-    private GGoodsProductDtoMapper gGoodsProductDtoMapper;
-    @Resource
-    private GGoodsDtoMapper gGoodsDtoMapper;
 
     @Test
     public void test4() {
 
 
-
-
-        GGoodsProductDto dto = gGoodsProductDtoMapper.selectOneByExampleSelective(
-                GGoodsProductDtoExample.build()
-                        .andSpecificationsEqualTo(new String[]{"标准"})
+        GTestDto dto = gTestDtoMapper.selectOneByExampleSelective(
+                GTestDtoExample.build()
+//                        .andSpecificationsEqualTo(new String[]{"标准"})
                         .example(),
-                GGoodsProductDto.Column.all()
+                GTestDto.Column.all()
         );
 
         System.out.println(dto);
 
 
-        List<Map> maps = gGoodsProductDtoMapper.selectToMap(
-                GGoodsProductDtoExample.build()
-                        .andSpecificationsEqualTo(new String[]{"标准"})
+        List<Map> maps = gTestDtoMapper.selectToMap(
+                GTestDtoExample.build()
+//                        .andSpecificationsEqualTo(new String[]{"标准"})
                         .example(),
-                GGoodsProductDto.Column.id.as()
+                GTestDto.Column.id.as()
         );
 
         System.out.println(maps);
